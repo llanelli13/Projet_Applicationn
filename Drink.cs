6c7c8b5972f852c6a7927cb7a555e58581d70e21
vexus95 <vexus95@outlook.fr>
2023-10-21 14:14:01 +0200

    Ajout drink

--- Drink.cs
+++ Drink.cs
@@ -5,7 +5,34 @@ using System.Collections.Generic;
 class Drink
 {
 
-    public string Name {get;set;}
+    public DrinkName Name {get;set;}
     public double Price {get;set;}
 
+    public Drink(DrinkName drinkName)
+    {
+        this.Name = drinkName;
+        switch (drinkName)
+        {
+            case drinkName.Water:
+                Price = 0.70;
+                break;
+            case drinkName.Coca:
+                Price = 1.00;
+                break;
+            case drinkName.IceTea:
+                Price = 1.00;
+                break;
+            case drinkName.Fanta:
+                Price = 1.00;
+                break;
+        }
+    }
+
+    public enum DrinkName
+    {
+        Water,
+        Coca,
+        IceTea,
+        Fanta
+    }
 }
