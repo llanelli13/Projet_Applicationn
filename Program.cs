3d850de8c98922bd7c5a7cd50fcabfd6723d658b
llanelli13 <pierre.rey@efrei.net>
2023-10-21 16:23:46 +0200

    r

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿class Program{
 
     static void Main(){
-        Clerk c1 = new Clerk("Pierre");
+        Clerk2 c1 = new Clerk2("Pierre");
         Customer custom = new Customer("Pierre","REY", "10b av JB Fortin", "0689883338");
         c1.AddCustomer(custom);
         c1.CheckingCustomer();
